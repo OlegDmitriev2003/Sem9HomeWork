dd3d291b7041c3827919e0039ac29795b26af49a
OlegDmitriev <odimitrovgrad@gmail.com>
2023-06-21 11:54:38 +0300

    completed Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+
+int schet (int n)
+{
+if (n == 0)
+    {
+        return n;
+    }
+Console.Write($"{n} ");
+return schet (n-1);
+}
+
+schet(n);
