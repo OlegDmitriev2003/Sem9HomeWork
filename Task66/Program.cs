51240d2d8f502da7aeb0fcf7fba348a7926fb5df
OlegDmitriev <odimitrovgrad@gmail.com>
2023-06-21 12:53:17 +0300

    completed Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+int summ = 0;
+
+
+int sum (int a, int b)
+{
+        if (a == b)
+    {
+        summ = summ + a;
+        return summ;
+    }
+summ = summ + a + b;
+return sum (a + 1, b - 1);
+}
+
+
+
+
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a<b)
+{
+    sum (a, b);
+}
+else if (a>b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+    sum (a, b);
+}
+Console.WriteLine($"Сумма натуральных чисел в промежутке между заданными числами - {summ}");
