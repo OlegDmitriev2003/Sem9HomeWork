22da2355e14ec9e290a8be5f7bfe65e5b87dc1b5
OlegDmitriev <odimitrovgrad@gmail.com>
2023-06-21 13:10:41 +0300

    completed Task68

---
+++ Task68/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+int levi (int n, int m)
+{
+    if (n==0)
+    {
+        return m + 1;
+    }
+    else if (m == 0)
+    {
+        return levi ( n - 1, 1);
+    }
+    else 
+    {
+        return levi (n-1, levi (n, m - 1));
+    }
+}
+
+Console.Write("Введите первое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Значение функции Аккермана для заданных чисел - {levi(n,m)}");
